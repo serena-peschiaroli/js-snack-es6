1a132bf294d4d9dcdc148e7609a71f7553878986
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-08 16:37:49 +0100

    Add: snack-one completed

--- snack-one/js/script.js
+++ snack-one/js/script.js
@@ -35,25 +35,62 @@ console.log(bici);
 const [firstBike, ...restOfBikes] = bici;
 console.log("First bike:", firstBike.nome, firstBike.peso);
 
-let biciLeggera;
 
-bici.forEach (({ nome, peso}) =>{
-    if (!biciLeggera || peso < biciLeggera.peso) {
-        biciLeggera = { nome, peso};
-    }
-});
-
-console.log("la bici più leggera è:", biciLeggera.nome, biciLeggera.peso);
+/*console.log("la bici più leggera è:", biciLeggera.nome, biciLeggera.peso);*/
 
 function stampaLeBiciLeggere(obj, pesoLimite) {
     console.log(`Biciclette con peso < ${pesoLimite}:`);
     obj.forEach(({ nome, peso }) => {
       if (peso < pesoLimite) {
         console.log(`${nome} - ${peso}`);
+        
       }
     });
 }
   
 stampaLeBiciLeggere(bici, 0.98);
   
+
+
+function generateBikeCard(bici) {
+    return `
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                     <h5 class="card-title">${bici.nome}</h5>
+                     <p class="card-text">${bici.peso}</p>
+                </div>
+            </div>    
+        </div>
+    `;
+}
+
+const rowElem = document.querySelector(".row");
+let biciElems = "";
+
+for (let i = 0; i < bici.length; i++) {
+    biciElems += generateBikeCard(bici[i]);
+}
+
+rowElem.innerHTML = biciElems;
+
+let biciLeggera;
+
+bici.forEach(({ nome, peso }) => {
+    if (!biciLeggera || peso < biciLeggera.peso) {
+        biciLeggera = { nome, peso };
+    }
+});
+
+
+const curBiciLeggera = document.querySelector(".leggera-bici");
+curBiciLeggera.innerHTML = generateBikeCard(biciLeggera);
+
+
+ 
+
+
+
+
+
 
